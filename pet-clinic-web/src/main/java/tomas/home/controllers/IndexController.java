69803b297f4d4aa860c2bc0363459bc611cecb8c
Tomasz Skrzypek <tomas14@uia.no>
2019-02-07 16:02:16 +0100

    Adding Index Page and Controller closes #8

---
+++ pet-clinic-web/src/main/java/tomas/home/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package tomas.home.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping({"","/","index","idex.html"})
+    public String index(){
+        return "index";
+    }
+}
