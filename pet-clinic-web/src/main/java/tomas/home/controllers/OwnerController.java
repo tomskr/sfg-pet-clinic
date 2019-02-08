6aa4e8938bb2a95246335125788f197f0b968641
Tomasz Skrzypek <tomas14@uia.no>
2019-02-08 12:19:07 +0100

    Added Owner List And Upgrade Conteroller. Closes#16

--- pet-clinic-web/src/main/java/tomas/home/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/tomas/home/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package tomas.home.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tomas.home.sfgpetclinic.services.OwnerServices;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
 
+    private final OwnerServices ownerServices;
+
+    public OwnerController(OwnerServices ownerServices) {
+        this.ownerServices = ownerServices;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners",ownerServices.findAll());
         return "owners/index";
     }
 }
