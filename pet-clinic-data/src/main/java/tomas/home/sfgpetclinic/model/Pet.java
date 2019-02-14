32c50c38969af77c2b1d66f5efecaf4c89cdca19
Tomasz Skrzypek <tomas14@uia.no>
2019-02-14 10:58:51 +0100

    Added Visit Entity, closes #38

--- pet-clinic-data/src/main/java/tomas/home/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/tomas/home/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package tomas.home.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -21,6 +23,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
