378d2818b8a4f0972309c92909b069add22adc69
Tomasz Skrzypek <tomas14@uia.no>
2019-02-28 19:52:26 +0100

    Added Project Lombok to project, Closes #51

--- pet-clinic-data/src/main/java/tomas/home/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/tomas/home/sfgpetclinic/model/Speciality.java
@@ -1,23 +1,21 @@
 package tomas.home.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
 
 }
